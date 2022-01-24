ba19d13dbfa22afe4f2438e33c03a4ca6f6a725f
Joe Futrelle <joefutrelle@gmail.com>
2022-01-24 14:28:56 -0500

    fixing double-slash in generated API URL

--- python/env/manualclassify/classify/utils.py
+++ python/env/manualclassify/classify/utils.py
@@ -193,7 +193,7 @@ def convertClassifierName(original):
 def getBinsInRange(start, end, timeseries):
     start = start.strftime('%Y-%m-%dT%H:%M:00')
     end = end.strftime('%Y-%m-%dT%H:%M:00')
-    url = timeseries + '/api/feed/temperature/start/' + start + '/end/' + end
+    url = timeseries + 'api/feed/temperature/start/' + start + '/end/' + end
     bins = []
     with closing(requests.get(url, stream=True)) as r:
         data = json.loads(r.text)
